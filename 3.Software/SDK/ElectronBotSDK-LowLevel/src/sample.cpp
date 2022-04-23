0258566716097f713593994fffdf3dd8a461e966
coolflyr_reg <coolflyr_reg@126.com>
2022-04-23 18:20:14 +0800

    添加linux的编译

--- 3.Software/SDK/ElectronBotSDK-LowLevel/src/sample.cpp
+++ 3.Software/SDK/ElectronBotSDK-LowLevel/src/sample.cpp
@@ -8,17 +8,7 @@ using namespace std;
 
 int main()
 {
-    // int USB_VID = 0x1001;
-    // int USB_PID = 0x8023;
-    // int devNum = USB_ScanDevice(USB_PID, USB_VID);
-
-    // while(true) {
-    //     printf("devNum: %d\n", devNum);
-    //     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-    //     devNum = USB_ScanDevice(USB_PID, USB_VID);
-    // }
-
+#if 1
     ElectronLowLevel robot;
 
     while (!robot.Connect()) {
@@ -29,25 +19,63 @@ int main()
 
     printf("Robot connected!\n");
 
-    VideoCapture video("video.mp4");
+    VideoCapture video;//("video.mp4");
     Mat frame;
 
+    // if (!video.open("video.mp4")) {
+    //     printf("file open failed!\n");
+    // }
+    if (video.isOpened()) {
+        printf("file is opened!\n");
+    } else {
+        printf("file is not open!\n");
+    }
 
     while (true)
     {
         video >> frame;
-        if (frame.empty())
+        if (frame.empty()){
+            printf("video frame is empty!\n");
+            robot.SetImageSrc(string("img.jpg"));
+            robot.Sync();
             break;
+        }
+            
 
         robot.SetImageSrc(frame);
         robot.Sync();
+    }
+    
+    for (int i = 0; i < 3; i++) {
+        robot.SetImageSrc(string("img.png"));
+        robot.Sync();
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+        robot.SetImageSrc(string("img.jpg"));
+        robot.Sync();
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
 
     robot.Disconnect();
     printf("File play finished, robot Disconnected!\n");
     printf("Press [Enter] to exit.\n");
-
     getchar();
     return 0;
+#else
+
+    cv::Mat image1;
+        cv::Mat image2;
+    image1 = cv::imread("img.jpg");
+    std::vector<cv::Mat> SplitChannel;
+    cv::split(image1,SplitChannel);
+    SplitChannel[1] == 0;
+    cv::merge(SplitChannel,image2);
+    cv::namedWindow("NO GREEN CHANNEL");
+    cv::imshow("results",image2);
+    cv::waitKey();
+    return 0;
+#endif
 }
 
